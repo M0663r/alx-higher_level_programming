14f2ade2563d7db3a3f2b92935f9aa3e96de6c5f
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-09-10 12:05:30 -0700

    #

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import random
+number = random.randint(-10000, 10000)
+
+print(f'Last digit of {number} is {number[-1]} and is', end=" ")
+if number[-1] == 0:
+    print("0"}
+elif number[-1] > 5:
+    print("greater than 5")
+elif number[-1] < 6 and number[-1] != 0:
+    print("less than 6 and not 0")
