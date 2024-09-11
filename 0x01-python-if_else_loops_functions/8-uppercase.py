e45c93c4ae1e714c1bcc0ffbe79c094438e58be6
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-09-11 13:42:12 -0700

    #

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:  # Check if char is lowercase
+            result += chr(ord(char) - 32)  # Convert to uppercase
+        else:
+            result += char
+    print("{}".format(result))
