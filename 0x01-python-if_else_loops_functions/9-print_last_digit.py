93ba25d044bb041ca238fa2fe32c0affc9ed4b6e
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-09-11 13:45:57 -0700

    #

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the last digit, handle negative numbers using abs()
-    print("{}".format(last_digit), end="")  # Print the last digit
-    return last_digit  # Return the last digit
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
