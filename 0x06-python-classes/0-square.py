7fde7cf80431456e1406abb62d10bec354a47354
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-10-03 09:18:47 -0700

    #

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class `Square` that represents a square.
+"""
+
+
 class Square:
+    """
+    This is an empty class that defines a square.
+    It currently does not contain any attributes or methods.
+    """
     pass
