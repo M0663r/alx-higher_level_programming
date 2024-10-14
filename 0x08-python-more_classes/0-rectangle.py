d9b6e592c83e9711fa3683bb64deb00547a85f24
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-10-14 02:02:04 -0700

    #

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class `Rectangle`.
+The class is intended to represent a rectangle.
+No attributes or methods are defined yet.
+"""
+
 class Rectangle:
     """Empty class that defines a rectangle."""
     pass
